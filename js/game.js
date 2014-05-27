1171b64d4afe0d88d05c8e44efd4c103a4f20274
Joe Haines <hello@joehaines.co.uk>
2014-05-27 17:09:09 +0100

    inventory interface mostly done

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   hpDisplay = document.getElementById("hpDisp")
   mpDisplay = document.getElementById("mpDisp")
+  armDisplay = document.getElementById("armDisp")
   strDisplay = document.getElementById("strDisp")
   agiDisplay = document.getElementById("agiDisp")
   magDisplay = document.getElementById("magDisp")
